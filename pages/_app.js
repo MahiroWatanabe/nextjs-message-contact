ae7de51bf9442f8ef815f52553d146595986a017
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-29 15:26:14 +0900

    change for get user information

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,29 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase";
 import Login from "./login";
 import Loading from "@/components/Loading";
+import { useEffect } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 export default function App({ Component, pageProps }) {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      db.collection("user").doc(user.uid).set(
+        {
+          email: user.email,
+          lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        {
+          // margeを指定することで、更新が必要なプロパティだけを更新
+          merge: true,
+        }
+      );
+    }
+  }, [user]);
 
   if (loading) return <Loading />;
   if (!user) return <Login />;
