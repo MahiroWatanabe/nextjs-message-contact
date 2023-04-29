b48ded7326179684e284a45c2a50399b7765aee8
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-29 21:59:33 +0900

    chat.js implementation

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,35 @@
+import { auth, db } from "@/firebase";
+import getRecipientEmail from "@/utils/getRecipientEmail";
 import { Avatar } from "@mui/material";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 import styled from "styled-components";
 
 const Chat = ({ id, users }) => {
+  const router = useRouter();
+  const [user] = useAuthState(auth);
+  const [recipientSnapshot] = useCollection(
+    db.collection("users").where("email", "==", getRecipientEmail(users, user))
+  );
+
+  const enterChat = () => {
+    router.push(`/chat/${id}`);
+  };
+
+  // chat相手のアイコン取得
+  const recipient = recipientSnapshot?.docs[0]?.data();
+  // chat相手のメールアドレス取得
+  const recepientEmail = getRecipientEmail(users, user);
   return (
-    <Container>
-      <UserAvatar />
-      <p>Recepient Email</p>
+    <Container onClick={enterChat}>
+      {recipient ? (
+        <UserAvatar src={recipient?.photoURL} />
+      ) : (
+        //   photoが設定されていない場合はchat相手の頭文字を設定
+        <UserAvatar>{recepientEmail[0]}</UserAvatar>
+      )}
+      <p>{recepientEmail}</p>
     </Container>
   );
 };
