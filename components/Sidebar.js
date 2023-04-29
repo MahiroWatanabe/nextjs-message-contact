11f425777644a98d1d4bca24abc70056395863b8
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-29 15:18:20 +0900

    create chat function

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,22 +2,45 @@ import { Avatar, Button, IconButton } from "@mui/material";
 import styled from "styled-components";
 import { Chat, SearchOutlined } from "@mui/icons-material";
 import { MoreVert } from "@mui/icons-material";
-import * as EmailValidator from "email-validate";
+import EmailValidator from "email-validator";
+import { auth, db } from "@/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import ChatUser from "./Chat";
 
 const Sidebar = () => {
+  const [user] = useAuthState(auth);
+  const useChatRef = db
+    .collection("chats")
+    .where("users", "array-contains", user.email);
+  const [chatSnapshot] = useCollection(useChatRef);
   const createChat = () => {
     const input = prompt(
       "Please enter an email address for the user you wish to chat with"
     );
 
     if (!input) return null;
-    if (EmailValidator.validate(input)) {
+    if (
+      EmailValidator.validate(input) &&
+      !chatAlreadyExists(input) &&
+      input !== user.email
+    ) {
+      db.collection("chats").add({
+        users: [user.email, input],
+      });
     }
   };
+
+  // chatがある場合はtrueを返す
+  const chatAlreadyExists = (recipientEmail) =>
+    !!chatSnapshot?.docs.find(
+      (chat) =>
+        chat.data().users.find((user) => user === recipientEmail)?.length > 0
+    );
   return (
     <Container>
       <Header>
-        <UserAvatar />
+        <UserAvatar onClick={() => auth.signOut()} />
         <IconsContainer>
           <IconButton>
             <Chat />
@@ -32,6 +55,9 @@ const Sidebar = () => {
         <SearchInput placeholder="Search in chats" />
       </Search>
       <SideBarButton onClick={createChat}>Start a new chat</SideBarButton>
+      {chatSnapshot?.docs.map((chat) => (
+        <ChatUser key={chat.id} id={chat.id} users={chat.data().users} />
+      ))}
     </Container>
   );
 };
