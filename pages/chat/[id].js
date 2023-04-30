e0eb4534aa13dd857723b457b5ee582e0c7e1ec9
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-30 15:36:41 +0900

    create [id].js

---
+++ pages/chat/[id].js
@@ -0,0 +1,49 @@
+import ChatScreen from "@/components/ChatScreen";
+import Sidebar from "@/components/Sidebar";
+import Head from "next/head";
+import styled from "styled-components";
+import { auth, db } from "@/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import getRecipientEmail from "@/utils/getRecipientEmail";
+
+const ChatPage = ({ chat, messages }) => {
+  const [user] = useAuthState(auth);
+  return (
+    <Container>
+      <Head>
+        <title>Chat with {getRecipientEmail(chat.users, user)}</title>
+      </Head>
+      <Sidebar />
+      <ChatContainer>
+        <ChatScreen chat={chat} messages={messages} />
+      </ChatContainer>
+    </Container>
+  );
+};
+
+export default ChatPage;
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      messages: messages,
+      chat: chat,
+    },
+  };
+}
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ChatContainer = styled.div`
+  flex: 1;
+  overflow: scroll;
+  height: 100vh;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
