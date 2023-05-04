6bb0c1326c6cd54a58c38718476d35e705978dea
MahiroWatanabe <konchikuwa2@gmail.com>
2023-05-04 10:36:47 +0900

    completion of ChatScreen.js

--- components/GroupChatScreen.js
+++ components/GroupChatScreen.js
@@ -1,9 +1,186 @@
+import { auth, db } from "@/firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import { AttachFile, InsertEmoticon, Mic, MoreVert } from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+import { useRef, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import { useRouter } from "next/router";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Message from "./Message";
+import TimeAgo from "timeago-react";
 
-const GroupChatScreen = () => {
-  return <Container></Container>;
+const GroupChatScreen = ({ chat, messages }) => {
+  const [user] = useAuthState(auth);
+  const [input, setInput] = useState("");
+  const endOfMessageRef = useRef(null);
+  const router = useRouter();
+
+  const [messagesSnapshot] = useCollection(
+    db
+      .collection("room")
+      .doc(router.query.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+  );
+  const latestMessage =
+    messagesSnapshot?.docs[messagesSnapshot.docs.length - 1].data();
+
+  const showMessages = () => {
+    if (messagesSnapshot) {
+      return messagesSnapshot.docs.map((message) => (
+        <Message
+          key={message.id}
+          user={message.data().user}
+          message={{
+            ...message.data(),
+            timestamp: message.data().timestamp?.toDate().getTime(),
+          }}
+        />
+      ));
+    } else {
+      return JSON.parse(messages).map((message) => {
+        <Message key={message.id} user={message.user} message={message} />;
+      });
+    }
+  };
+
+  const scrollToBottom = () => {
+    endOfMessageRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    //   update user timestamp
+    db.collection("users").doc(user.uid).set(
+      {
+        lastSeen: firebase.firestore.FieldValue.serverTimestamp(),
+      },
+      { merge: true }
+    );
+
+    db.collection("room").doc(router.query.id).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user.email,
+      photoURL: user.photoURL,
+    });
+
+    setInput("");
+    scrollToBottom();
+  };
+
+  return (
+    <Container>
+      <Header>
+        <Avatar sx={{ bgcolor: chat.iconColor }}>{chat.groupname[0]}</Avatar>
+        <HeaderInformation>
+          <h3>{chat.groupname}</h3>
+          {messagesSnapshot ? (
+            <p>
+              Last active:{" "}
+              {latestMessage?.timestamp?.toDate() ? (
+                <TimeAgo datetime={latestMessage?.timestamp?.toDate()} />
+              ) : (
+                "Unavailable"
+              )}
+            </p>
+          ) : (
+            <p>Loading last active...</p>
+          )}
+        </HeaderInformation>
+        <HeaderIcons>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </HeaderIcons>
+      </Header>
+
+      <MessageContainer>
+        {showMessages()}
+        <EndOfMessage ref={endOfMessageRef} />
+      </MessageContainer>
+
+      <InputContainer>
+        <InsertEmoticon />
+        <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button hidden disabled={!input} type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
+        <Mic />
+      </InputContainer>
+    </Container>
+  );
 };
 
 export default GroupChatScreen;
 
 const Container = styled.div``;
+
+const Input = styled.input`
+  flex: 1;
+  outline: 0;
+  border: none;
+  border-radius: 10px;
+  align-items: center;
+  padding: 20px;
+  margin-left: 15px;
+  margin-right: 15px;
+  background-color: whitesmoke;
+`;
+
+const InputContainer = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  position: sticky;
+  bottom: 0;
+  background-color: white;
+  z-index: 100;
+`;
+
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  padding: 11px;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+  > h3 {
+    margin-bottom: 3px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`;
+
+const HeaderIcons = styled.div``;
+
+const MessageContainer = styled.div`
+  padding: 30px;
+  background-color: #e5ded8;
+  min-height: 90vh;
+`;
+
+const EndOfMessage = styled.div`
+  margin-bottom: 50px;
+`;
