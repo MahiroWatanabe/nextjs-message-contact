975df75c8b6e0b591f8f6c5e9e375bbc338b8e1a
MahiroWatanabe <konchikuwa2@gmail.com>
2023-05-12 18:05:08 +0900

    add domains to display images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["assets.stickpng.com"],
+    domains: [
+      "assets.stickpng.com",
+      "s3-media1.fl.yelpcdn.com",
+      "s3-media2.fl.yelpcdn.com",
+      "s3-media3.fl.yelpcdn.com",
+      "s3-media4.fl.yelpcdn.com",
+      "s3-media5.fl.yelpcdn.com",
+    ],
   },
 };
 
