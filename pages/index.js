a8b8e4f465d4569025d1e691845a4a2c21913174
MahiroWatanabe <konchikuwa2@gmail.com>
2023-05-12 18:22:09 +0900

    change to use useContext

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Sidebar from "@/components/Sidebar";
+import { BooleanProvider } from "@/contexts/displayContext";
 import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Message-Contact</title>
-      </Head>
-      <Sidebar />
+      <BooleanProvider>
+        <Head>
+          <title>Message-Contact</title>
+        </Head>
+        <Sidebar />
+      </BooleanProvider>
     </div>
   );
 }
